eb5e436102cd5b5461e5282aaf7411dada6f74eb
Adityaraj Pal <paladityaraj353@gmail.com>
2026-01-14 02:34:44 +0530

    landing

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,10 @@
 import httpStatus from 'http-status';
-import {User} from '../models/user.model.js';
+import {User} from '../models/user.models.js';
 import bcrypt, { hash } from "bcrypt"
+
+
+import crypto from "crypto"
+import { Meeting } from "../models/meeting.model.js";
 
 const login=async (req,res)=>{
     const {username,password}=req.body;
@@ -22,7 +26,8 @@ const login=async (req,res)=>{
         else{
             return res.status(httpStatus.UNAUTHORIZED).json({message:"Invalid Username or password"})
         }
-    } catch(e){
+    }
+    catch(e){
         return res.status(500).json({message:`Something went wrong ${e}`})
     }
 }
@@ -35,7 +40,6 @@ const register=async(req,res)=>{
             return res.status(httpStatus.FOUND).json({ message: "User already exists" });
         }
         const hashedPassword=await bcrypt.hash(password,10);
-
         const newUser=new User({
             name: name,
             username: username,
@@ -43,41 +47,36 @@ const register=async(req,res)=>{
         });
         await newUser.save();
         res.status(httpStatus.CREATED).json({message:"User Registered"})
-
     }
     catch(e){
         res.json({message:`Something went wrong ${e}`})
     }
 }
 
-const getUserHistory = async (req, res) => {
-    const { token } = req.query;
-
-    try {
-        const user = await User.findOne({ token: token });
-        const meetings = await Meeting.find({ user_id: user.username })
+const getUserHistory=async(req,res)=>{
+    const {token}=req.query;
+    try{
+        const user = await User.findOne({token:token});
+        const meetings = await Meeting.find({user_id:user.username})
         res.json(meetings)
-    } catch (e) {
-        res.json({ message: `Something went wrong ${e}` })
+    }
+    catch(e){
+        res.json({message:`Something went wrong ${e}`})
     }
 }
-
-const addToHistory = async (req, res) => {
-    const { token, meeting_code } = req.body;
-
-    try {
-        const user = await User.findOne({ token: token });
-
-        const newMeeting = new Meeting({
+const addToHistory=async(req,res)=>{
+    const{token,meeting_code }=req.body;
+    try{
+        const user=await User.findOne({token:token});
+        const newMeeting=new Meeting({
             user_id: user.username,
-            meetingCode: meeting_code
+            meetingCode:meeting_code
         })
-
         await newMeeting.save();
-
-        res.status(httpStatus.CREATED).json({ message: "Added code to history" })
-    } catch (e) {
-        res.json({ message: `Something went wrong ${e}` })
+        res.status(httpStatus.CREATED).json({message:"Added code to history"})
+    }
+    catch(e){
+        res.json({message:`Something went wrong ${e}`})
     }
 }
 export {login,register,getUserHistory,addToHistory}
---
+++ frontend/src/environment.js
@@ -0,0 +1,4 @@
+// Backend server configuration
+const server = process.env.REACT_APP_SERVER_URL || 'http://localhost:5000';
+
+export default server;
